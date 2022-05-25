14337b60a7a1fd1442d8bc4abcd695b67515c8dd
Alon Shani <alonshani16@gmail.com>
2022-05-25 21:09:42 +0300

    Player movement and shield implementation

---
+++ Assets/Scripts/Behaviours/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector3 mousePosition;
+    public float mouseSpeed = 0.1f;
+    Rigidbody2D rb;
+    public float speed = 6;
+    public bool shieldCooldowm = true;
+    public int cooldown = 10;
+    public float duration = 1.5f;
+    public GameObject shield;
+    Vector2 position = new Vector2(0f, 0f);
+
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+        position = Vector2.Lerp(transform.position, mousePosition, mouseSpeed);
+
+        // Player movement with keyboard
+        float horizontalMove = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.position += new Vector3(horizontalMove, 0);
+
+        float verticalmMove = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.position += new Vector3(0, verticalmMove);
+
+        // Activate shield
+        if (Input.GetKeyDown(KeyCode.Mouse1) && shieldCooldowm == true)
+        {
+            shield.SetActive(true);
+            StartCoroutine(Deactivate());
+            StartCoroutine(Cooldown());
+            shieldCooldowm = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(position);
+    }
+
+    IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(cooldown);
+        shieldCooldowm = true;
+    }
+
+    IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(duration);
+        shield.SetActive(false);
+    }
+}
